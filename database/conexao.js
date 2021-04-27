f20d1d1cb316e4c1bf2f8c2bec4cf6ab4c36af7e
Fernanda Pieri <nandapieri@gmail.com>
2021-04-27 09:18:03 -0300

    conectado com o banco de dados mysql

---
+++ database/conexao.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql2');
+require('dotenv/config');
+
+const conexao = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  port: 3306,
+  password: process.env.DB_PASS,
+  database: 'agenda-petshop'
+});
+
+module.exports = conexao;
--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 const customExpress = require('./config/customExpress');
+const conexao = require('./database/conexao')
 
-const app = customExpress();
-app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+conexao.connect( (error) => {
+  if(error) {
+    console.log(error);
+  } else {
+    console.log('Banco de dados conectado');
+    const app = customExpress();
+    app.listen(3000, () => console.log('Servidor rodando na porta 3000'));
+  }
+});
