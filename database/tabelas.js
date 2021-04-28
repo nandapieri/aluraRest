e23aadf9b0b3dbb04997cb37f0244ab47a6f77be
Fernanda Pieri <nandapieri@gmail.com>
2021-04-28 15:47:59 -0300

    adicionado servico de busca por cpf e alteração na tabela de atendimento para armazenar cpf não nome do cliente

--- database/tabelas.js
+++ database/tabelas.js
@@ -7,7 +7,7 @@ class Tabelas {
 
   criaAtendimentos() {
     const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, ' +
-      'cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, ' +
+      'cliente varchar(11) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, ' +
       'data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, ' +
       'obs text, PRIMARY KEY(id))';
     this.conexao.query(sql, (error) => {
